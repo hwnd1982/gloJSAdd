e14ae477905b26bfc1c22a2d873bda8b5be7ae53
Kirill Lavrov <lkv@myttk.ru>
2021-07-30 21:37:25 +0700

    fix (script.js): Using template strings

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ let multArrayItems = String(num).split('').reduce((mult, current) => {
 // 3) Полученный результат multArrayItems возводим в степень 3;
 let expNum = multArrayItems ** 3;
 // 2) Выводим в консоль произведение (умножение) цифр этого числа num;
-console.log('Произведение цифр числа', num, '=', multArrayItems);
+console.log(`Произведение цифр числа ${num} = ${multArrayItems}`);
 // 4) Выводим в консоль первые 2 цифры полученного числа
-console.log('Результата возведения в степень 3 произведения цифр числа', 
-num,'=', expNum, 'Первые 2 цифры полученного результата =', Number(String(expNum).substr(0, 2)));
-
-
+console.log(`Результата возведения в степень 3 произведения цифр числа ${num} = ${expNum}
+Первые 2 цифры полученного результата = ${Number(String(expNum).substr(0, 2))}`);
