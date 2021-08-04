0ee978d4e8916cec86a81f0964349096a19ddd23
Kirill Lavrov <lkv@myttk.ru>
2021-08-04 14:30:18 +0700

    feat (script.js): Additional task completed

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const getFormattedString = (str) => {
+  if (typeof str !== 'string') {
+    return 'Ошибка: переданые данные не являются строкой!'
+  } else {
+    if (!str.trim()) {
+      return 'Ошибка: передана пустая строка!'
+    } else {
+      return str.trim().length <=30 ? str.trim() : str.trim().slice(0,30) + '...';
+    }
+  }
+};
+
+console.log(getFormattedString(prompt('Введите текст сообщения:')));
