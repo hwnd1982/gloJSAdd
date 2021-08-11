a19f12ce3de091e13d2d9e5081bcbf3fa5180b82
Kirill Lavrov <lkv@myttk.ru>
2021-08-11 15:31:32 +0700

    feat (script.js): Additional task completed

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const
+today = new Date(),
+daysOfWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+for (let i = 0; i < daysOfWeek.length; i++) {
+  let day = i + 1 > 5 ? daysOfWeek[i].italics() : daysOfWeek[i];
+  
+  document.body.appendChild(document.createElement('span')).innerHTML =
+    `<br>${i + 1 !== today.getDay() ? day : day.bold()}`;
+}
