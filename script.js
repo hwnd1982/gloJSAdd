47577e3ce454ce91e0b837b1bbdb6e9e7fdb4c68
Kirill Lavrov <lkv@myttk.ru>
2021-08-25 20:41:11 +0700

    feat (script.js): Additional task completed

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const getRendomColor = () => {
+  return '#'+ ('000000' + (Math.random()*0x1000000).toString(16)).slice(-6);
+},
+setBgColor = (event) => {
+  let color = getRendomColor();
+  document.querySelector('.color-value').textContent = color;
+  document.querySelector('.chenge-color').style.color = color;
+  document.body.style.backgroundColor = color;
+};
+
+setBgColor();
+document.querySelector('.chenge-color').addEventListener('click', setBgColor);
