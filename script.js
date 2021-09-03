6bbc598dfe7b8f0e03ee9f6108b8aca884e2be48
Kirill Lavrov <lkv@myttk.ru>
2021-09-03 22:10:30 +0700

    fix (script.js): Fix eslint

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
-'use strict';
+
 
 const
   newYearTimerElem = document.body.appendChild(document.createElement('p')),
-  getDeclensionOfStringByNumber = (num, expressions) => { 
+  getDeclensionOfStringByNumber = (num, expressions) => {
     switch (true) {
-      case +((num += '').substr(-2)) > 10  &&  
-        +((num += '').substr(-2)) <= 20 : 
-          return num  + ' ' + expressions[2];
-      case num % 10 === 0: return num  + ' ' + expressions[2];
-      case num % 10 === 1: return num  + ' ' + expressions[0];
-      case num % 10 < 5: return num  + ' ' + expressions[1];
-      default: return num  + ' ' + expressions[2];
+    case +((num += '').substr(-2)) > 10  &&
+        +((num += '').substr(-2)) <= 20 :
+      return num  + ' ' + expressions[2];
+    case num % 10 === 0: return num  + ' ' + expressions[2];
+    case num % 10 === 1: return num  + ' ' + expressions[0];
+    case num % 10 < 5: return num  + ' ' + expressions[1];
+    default: return num  + ' ' + expressions[2];
     }
   },
-  getFormattedNumber = (num) => {
-    return num > 9 ? ('' + num)  : ('0' + num);
-  },
-  setStyle = (elem) => {
+  setStyle = elem => {
     elem.style.fontSize = '18px';
     elem.style.fontStyle = 'italic';
     elem.style.fontFamily = 'Roboto, sans-serif';
   },
-  newYearTimer = (elem) => {
-    const 
+  newYearTimer = elem => {
+    const
       today = new Date(), timeOfDay = today.getHours(),
-      newYear = new Date(`Dec 31, ${today.getFullYear()}`),
-      daysLeft = Math.ceil((newYear - today) / (1000*3600*24));
-    let 
-      greeting = '',
-      date = today.toLocaleDateString("ru", {weekday: "long"});
+      newYear = new Date(today.getFullYear(), 11, 31),
+      daysLeft = Math.ceil((newYear - today) / (1000 * 3600 * 24)),
+      date = today.toLocaleDateString("ru", { weekday: "long" });
+    let greeting = '';
 
     switch (true) {
-      case timeOfDay >= 4 && timeOfDay <= 11: greeting = 'Доброе утро!'; break;
-      case timeOfDay >= 12 && timeOfDay <= 16: greeting = 'Добрый день!'; break;
-      case timeOfDay >= 17 && timeOfDay <= 22: greeting = 'Добрый вечер!'; break;
-      default: greeting = 'Доброй ночи!';
+    case timeOfDay >= 4 && timeOfDay <= 11: greeting = 'Доброе утро!'; break;
+    case timeOfDay >= 12 && timeOfDay <= 16: greeting = 'Добрый день!'; break;
+    case timeOfDay >= 17 && timeOfDay <= 22: greeting = 'Добрый вечер!'; break;
+    default: greeting = 'Доброй ночи!';
     }
 
     setStyle(elem);
     elem.innerHTML = `${greeting}<br>
     Сегодня: ${date[0].toUpperCase() + date.slice(1)}<br>
-    Текущее время: ${today.toLocaleTimeString("en-US", {hour: "numeric", minute: "numeric", second: "numeric"})}
+    Текущее время: ${today.toLocaleTimeString("en-US", { hour: "numeric", minute: "numeric", second: "numeric" })}
     <br>До нового года осталось ${getDeclensionOfStringByNumber(daysLeft, ['день', 'дня', 'дней'])}`;
   };
 
