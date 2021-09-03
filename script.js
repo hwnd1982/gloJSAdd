d87989aa6ff5a565b331d965d7f168f594a8a253
Kirill Lavrov <lkv@myttk.ru>
2021-09-04 01:05:12 +0700

    feat (script.js): Additional task completed

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-'use strict';
+const debounce = (fn, ms) => {
+    let
+      lastCall = 0,
+      lastCallTimer = 0;
+
+    return (...args) => {
+      const prevCall = lastCall;
+      lastCall = Date.now();
+
+      if (prevCall && (lastCall - prevCall) < ms) {
+        clearInterval(lastCallTimer);
+      }
+      lastCallTimer = setTimeout(() => fn(...args), ms);
+    };
+  },
+  showText = value => document.getElementById('text').textContent = value,
+  showTextDebounce = debounce(showText, 300);
+
+document.getElementById('input').addEventListener('input', event => showTextDebounce(event.target.value));
