72b3d7dd39de06555304d047d3c4e9a8882fb762
Kirill Lavrov <lkv@myttk.ru>
2021-09-29 21:14:25 +0700

    fear (script.js): Retrieving Data with Promises

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-  const select = document.getElementById('cars'),
+  const
+    select = document.getElementById('cars'),
     output = document.getElementById('output');
 
   select.addEventListener('change', () => {
-    const request = new XMLHttpRequest();
-    request.open('GET', './cars.json');
-    request.setRequestHeader('Content-type', 'application/json');
-    request.send();
-    request.addEventListener('readystatechange', () => {
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
-        data.cars.forEach(item => {
-          if (item.brand === select.value) {
-            const { brand, model, price } = item;
-            output.innerHTML = `Тачка ${brand} ${model} <br>
-                        Цена: ${price}$`;
-          }
-        });
-      } else {
-        output.innerHTML = 'Произошла ошибка';
-      }
-    });
-  });
+    const
+      getData = () => new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
 
+        request.addEventListener('readystatechange', () => {
+          if (request.readyState !== 4) {
+            return;
+          }
+          request.status === 200 ?
+            resolve(JSON.parse(request.responseText)) :
+            reject('Произошла ошибка!');
+        });
+        request.open('GET', './cars.json');
+        request.setRequestHeader('Content-type', 'application/json');
+        request.send();
+      }),
+      showData = data => data.cars.forEach(({ brand, model, price }) => (brand === select.value ?
+        output.innerHTML = `Тачка ${brand} ${model} <br> Цена: ${price}$` : null)),
+      showError = error => output.innerHTML = error;
+
+    getData().then(showData).catch(showError);
+  });
 });
