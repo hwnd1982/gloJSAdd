e3efe372bf8e30335b44c42caf8087b022ee1a0d
Kirill Lavrov <lkv@myttk.ru>
2021-10-08 18:46:27 +0700

    fix (.all): Add sitactic sugar

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { DropdownHandler } from './modules/DropdownHandler';
 const
   overlay = new Overlay(),
   local = document.cookie.replace(/(local=)([A-Z]{2})/i, (match, key, value) => value);
-if (!['DE', 'EN', 'RU'].includes(local) || !localStorage.localData) {
-  overlay.showLocalSelectMenu(dataRequest, DropdownHandler);
-} else {
+!['DE', 'EN', 'RU'].includes(local) || !localStorage.localData ?
+  overlay.showLocalSelectMenu(dataRequest, DropdownHandler) :
   new DropdownHandler(JSON.parse(localStorage.localData), local);
-}
