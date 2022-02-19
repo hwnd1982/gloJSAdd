2fb869e33bee85ae3083572407f5b17d19d81694
Kirill Lavrov <lkv@myttk.ru>
2022-02-19 22:42:13 +0700

    faet (scropt.js) Added function Added mask function to the phone, but it needs to be rewritten!

--- script.js
+++ script.js
@@ -78,6 +78,48 @@ async function createHTMLFile() {
   if (ready) location = `./${value}.html`;
 }
 
+
+const maskInput = (selector, mask) => {
+  const elems = document.querySelectorAll(selector);
+  const masked = ({ target }) => {
+    let delta = 0;
+    const cursorPosition  = target.selectionStart;
+    target.value = mask.split('').map((char, index) => {
+      const value = target.value.split('');
+
+      if (value[index]) {
+        if (/\d/.test(value[index])) {
+          if (/[)(+\-.\s]/.test(char)) {
+            const reg = new RegExp(`[${char}][)(+\\-.\\s]*`);
+
+            char = mask.match(reg)[0];
+            if (index < cursorPosition) delta = char.length;
+            return char + value[index];
+          }
+        } else {
+          if (!/[)(+\-.\s]/.test(char))
+            return '';
+        }
+        return value[index];
+      } else return '';
+    }).join('');
+
+    target.selectionStart = cursorPosition + delta;
+    target.selectionEnd = cursorPosition + delta;
+  };
+  const focus = ({ target }) =>
+    (target.value.length <= mask.indexOf('_') ? mask.slice(0, mask.indexOf('_')) : target.value);
+  const blur = ({ target }) =>
+    (target.value.length <= mask.indexOf('_') ? '' : target.value);
+
+  elems.forEach(elem => {
+    elem.addEventListener("input", event => masked(event));
+    elem.addEventListener("focus", event => event.target.value = focus(event));
+    elem.addEventListener("blur", event => event.target.value = blur(event));
+  });
+};
+
+maskInput('input', '+7 (___) ___-__-__');
 document.getElementById('send').addEventListener('click', sendJSON);
 document.getElementById('edit').addEventListener('click', editJSON);
 document.getElementById('create').addEventListener('click', createHTMLFile);
