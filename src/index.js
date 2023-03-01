7d1f5f59f801f660a6cb05b5a0e26a73d7b2d214
Kirill Lavrov <lkv@myttk.ru>
2023-03-02 00:38:29 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/index.css";
 import "./index.html";
 
 import Storage from "./js/components/elements/Storage";
-import Frontend from "./js/components/profession/Frontend";
+
 import DomElement from "./js/components/elements/DomElement";
 import ClassSelect from "./js/components/elements/ClassSelect";
 import Form from "./js/components/elements/Form";
-import Worker from "./js/components/profession/Worker";
 import Table from "./js/components/elements/Table";
+
+import Worker from "./js/components/profession/Worker";
+import Frontend from "./js/components/profession/Frontend";
 import Runner from "./js/components/profession/Runner";
 
 const workerClasses = [Worker, Frontend, Runner];
@@ -18,6 +19,7 @@ const storage = new Storage("worker", workerClasses);
 const table = new Table(storage);
 const form = new Form(storage);
 const select = new ClassSelect(storage);
+
 const query = new DomElement("div", ["col-lg-12"], { children: [select.elem, form.elem] });
 const veiw = new DomElement("div", ["col-lg-12"], { children: [table.elem] });
 
